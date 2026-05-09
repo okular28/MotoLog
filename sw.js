2eefbc961c65092d6bcb686eaf5edbc01b960279
Okular28 <pawel.siola2003@gmail.com>
2026-05-09 16:03:40 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'motolog-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html'
+  '/',
+  '/index.html'
 ];
 
 self.addEventListener('install', event => {
@@ -37,8 +37,8 @@ self.addEventListener('push', event => {
   
   const options = {
     body: data.body,
-    icon: './favicon.svg',
-    badge: './favicon.svg',
+    icon: '/favicon.svg',
+    badge: '/favicon.svg',
     vibrate: [100, 50, 100],
     data: {
       dateOfArrival: Date.now(),
